9881a92ffd8456d6f5a4b6f321a28fb10489da55
decentralizedRK <decentralizedrk@gmail.com>
2022-07-12 14:25:19 +0900

    added solution

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Leetcode
 These are the problems with solutions I solved on leetcode.
+I solve problems almost daily and push it here .
+You can check my solutions and contribute if you have better solution.
--- twoSum/Solution.java
+++ twoSum/Solution.java
@@ -1,8 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-    }
-    public static void main(String[] args) {
-        
+        int []result =new int[2];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.containsKey(target-nums[i])){
+                result[0]=i;
+                result[1]=map.get(target-nums[i]);
+                break;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return result;
     }
 }
